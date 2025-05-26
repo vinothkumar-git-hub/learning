68e65dca885834da68968a349755962c97a88a9b
vinothkumar-git-hub <vinothkumar.r7895@gmail.com>
2025-05-26 18:28:46 +0530

    quotation types

---
+++ quotation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###Start###
+
+#Single Quotation ' '
+#Double Quotation " "
+#Revarse Quoation ` `
+
+var1=787896
+var2=learning
+
+#Single Quotation
+echo 'Single quotes $var1 $var2'
+
+#Double Quatation
+echo "Double quotes $var1 $var2"
+
+#Reverse Quotation
+echo "This Hostname is `hostname`"
+###End###
